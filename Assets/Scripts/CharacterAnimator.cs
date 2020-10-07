6494268abe223201af228a6f14bb56a65ca5732b
baraking <barakatz.bk@gmail.com>
2020-10-07 22:54:09 +0300

    all project
    doors, animations... all

---
+++ Assets/Scripts/CharacterAnimator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterAnimator : MonoBehaviour
+{
+
+    const float locomtionAnimationSmoothTime = .1f;
+
+    NavMeshAgent agent;
+    Animator animator;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        float speedPercent = agent.velocity.magnitude / agent.speed;
+        animator.SetFloat("speedPercent", speedPercent, locomtionAnimationSmoothTime, Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        animator.Play("DoorOpenAnimation");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        animator.Play("DoorCloseAnimation");
+    }
+}
